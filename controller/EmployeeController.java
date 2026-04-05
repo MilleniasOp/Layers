86e328778ae329888533d4e52cea1e0391ca1d68
bluebutterfly-123 <klperry517@gmail.com>
2026-04-05 17:20:29 -0500

    Update EmployeeController.java
    added calling functions for viewing tasks and updating task status

--- controller/EmployeeController.java
+++ controller/EmployeeController.java
@@ -1,5 +1,9 @@
 package controller;
 import controller.AttendanceController;
+import controller.TaskController;
+import entity.Task;
+import entity.Employee;
+import java.util.List;
 
 public class EmployeeController {
 
@@ -11,6 +15,16 @@ public class EmployeeController {
 
     public void clockOut(String username) {
         attendanceController.clockOut(username);
+    }
+
+    //Viewing tasks assigned to employee
+    public List<Task> viewTasksAssigned (Employee employee){
+        return TaskController.getTasksFromSupabase(employee);
+    }
+
+    //Updating the status of tasks
+    public void updateTaskStatus (Task task, Employee employee, String newStatus){
+        TaskController.updateTask (task,employee, newStatus);
     }
 
 }
